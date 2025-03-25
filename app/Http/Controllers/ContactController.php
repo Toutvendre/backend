ca7497da86c4aa9e1025a84ba635b7f07c21b196
Toutvendre <ducvfx07@gmail.com>
2025-03-25 14:53:57 +0000

    Premier commit

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function send(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email',
+            'message' => 'required|string',
+        ]);
+
+        return response()->json(['message' => 'Message reçu avec succès !'], 200);
+    }
+}
