ae422082bd24e49940c76d274e0a3969d522a394
Toutvendre <ducvfx07@gmail.com>
2025-03-25 15:10:06 +0000

    Premier tes

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
     public function send(Request $request)
     {
-        $request->validate([
+        $data = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email',
             'message' => 'required|string',
         ]);
 
-        return response()->json(['message' => 'Message reçu avec succès !'], 200);
+        Contact::create($data);
+
+        return response()->json(['message' => 'Message reçu et enregistré avec succès !'], 200);
     }
 }
